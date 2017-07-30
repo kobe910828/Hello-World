18f5c5c86904045ece855b6bb08403376e789d0b
kobe <13732263545@163.com>
2017-07-30 14:32:18 +0800

    add

--- hello.java
+++ hello.java
@@ -2,4 +2,9 @@ public hello{
 	public static void main(String[] args){
         System.out.println("Hello World!");
     }
+
+
+    public void f1(String str1, String str2){
+		System.out.println("one");
+    }
 }
--- hello1.java
+++ hello1.java
@@ -1,10 +1,80 @@
-public hello{
-	public static void main(String[] args){
+
+/**
+ * Class description
+ *
+ *
+ * @version        Enter version here..., 17/07/24
+ * @author         xin.huang
+ */
+public class hello {
+
+    /** Field description */
+    private String aa;
+
+    /** Field description */
+    private String bb;
+
+    /**
+     * Method description
+     *
+     *
+     * @param str1
+     * @param str2
+     *
+     * @return
+     */
+    public String f1(String str1, String str2) {
+        System.out.println("one");
+    }
+
+    /**
+     * Method description
+     *
+     *
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
         System.out.println("Hello World!");
     }
 
+    /**
+     * Method description
+     *
+     *
+     * @return String
+     */
+    public String getAa() {
+        return aa;
+    }
 
-    public void f1(String str1, String str2){
-		System.out.println("one");
+    /**
+     * Method description
+     *
+     *
+     * @param aa
+     */
+    public void setAa(String aa) {
+        this.aa = aa;
+    }
+
+    /**
+     * Method description
+     *
+     *
+     * @return String
+     */
+    public String getBb() {
+        return bb;
+    }
+
+    /**
+     * Method description
+     *
+     *
+     * @param bb
+     */
+    public void setBb(String bb) {
+        this.bb = bb;
     }
 }
